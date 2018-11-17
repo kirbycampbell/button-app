41e40cf36f631cfdefac8b5ce909b453d88d5a90
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-17 14:30:03 -0800

    Set up for tutorial

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  state = {};
+  render() {
+    return <button className="col-1">Press Me</button>;
+  }
+}
+
+export default Button;
